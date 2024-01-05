6dc7de2633a49bcacc43d2fb4345e993803b35c5
bibin-jaimon <bibinjaimon@gmail.com>
2024-01-06 01:22:27 +0530

    Initial Setup

---
+++ DiscordCloneSwiftUI/DiscordCloneSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiscordCloneSwiftUIApp.swift
+//  DiscordCloneSwiftUI
+//
+//  Created by Bibin Jaimon on 05/01/24.
+//
+
+import SwiftUI
+
+@main
+struct DiscordCloneSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
